25afa45331ca9f522d20575e146ec7f966b8e97f
allanternaverde <allanternaverde@gmail.com>
2018-07-04 18:13:19 -0300

    Update dispEO.m

--- dispEO.m
+++ dispEO.m
@@ -2,7 +2,7 @@ function [] = dispEO(a,e,I,Omega,omega,upsilon)
     disp('Elementos orbitais');
     disp(['a=',num2str(a)]);
     disp(['e=',num2str(e)]);
-    disp(['i=',num2str(i*180/pi), '°']);
+    disp(['I=',num2str(I*180/pi), '°']);
     disp(['Omega=',num2str(Omega*180/pi), '°']);
     disp(['omega=',num2str(omega*180/pi), '°']);
     disp(['upsilon=',num2str(upsilon*180/pi), '°']);
