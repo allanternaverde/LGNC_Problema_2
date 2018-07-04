16cbf69f33d150d9b276a3786f12cdfbcfa10627
allanternaverde <allanternaverde@gmail.com>
2018-07-04 18:12:57 -0300

    Update dispRV.m

--- dispRV.m
+++ dispRV.m
@@ -6,7 +6,7 @@ function [] = dispRV(R,V)
 
     % vetor de velocidade v 
     disp('Vetor velocidade')
-    s=sprintf('v= % +.3f I % +.3f J     % +.3f K',R(1),R(2),R(3));
+    s=sprintf('v= % +.3f I % +.3f J % +.3f K',R(1),R(2),R(3));
     disp(s)
     disp(' ');
 end
